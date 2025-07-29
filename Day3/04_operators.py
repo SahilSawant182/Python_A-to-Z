2725213bf7ca412c4bdf018722f38169467ec18e
sahil sawant <sawantsahil183@gmail.com>
2025-07-29 21:18:30 +0530

    Update 04_operators.py

--- Day3/04_operators.py
+++ Day3/04_operators.py
@@ -5,7 +5,7 @@ c = a + b
 print(c)
 
 # Assignment Operators
-a = 4-2 # Assign 4-2 in a
+a = 4-2 # Assign 4-2 in a 
 print(a)
 b = 6
 # b += 3 # Increment the value of b by 3 and then assign it to b
